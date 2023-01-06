176e493413f36e43a0bfcd88bf704c197962c356
OneDeadFox <tjosephb.pro@gmail.com>
2023-01-06 09:29:39 -0800

    added event listener for save button clicks, added dyynamic formatting for calander relative to current time, created function for storing and printing user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,64 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+
+  //Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
+
+  //Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+
+  //Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements.
+
+  //Add code to display the current date in the header of the page.
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  //Global Variables-------------------------------------------
+  let hourBlocks = $('.time-block');
+  let saveButtons = $('.saveBtn');
+  let scheduleInput = $('.description');
+
+  let todayDisplay = $('#currentDay');
+
+  todayDisplay.text(dayjs());
+
+  setTextValues();
+  setRelativeColors();
+
+  //Click Event Listener---------------------------------------
+  //listen for clicks on the save button
+  hourBlocks.on('click', '.saveBtn', function(e){
+    var hour = $(e.target).parent().attr('id');
+    var inputActual = $(e.target).siblings('.description').val();
+    
+    localStorage.setItem(`${hour}`, inputActual);
+    setTextValues();
+    //console.log(localStorage.getItem(`${hour}`));
+    
+  });
+
+  //Functions--------------------------------------------------
+  //set text area value based on stored input
+  function setTextValues(){
+    $.each(hourBlocks, function(){
+      $(this).children('.description').val(localStorage.getItem(`${$(this).attr('id')}`));
+    });
+  }
+
+  //color formatting
+  function setRelativeColors(){
+    $.each(hourBlocks, function(){
+      //I need to learn more about regEx
+      var idString = $(this).attr('id').replace(/\D/g, "");
+      var currentClass = $(this).attr('class');
+      var hourActual = parseInt(idString);
+      var hourCurrent = dayjs().format('H');
+
+      console.log(currentClass);
+
+      if(hourActual - hourCurrent < 0){
+        $(this).attr('class', `${currentClass} past `);
+      } else if(hourActual - hourCurrent === 0){
+        $(this).attr('class', `${currentClass} present`);
+      } else if(hourActual - hourCurrent > 0){
+        $(this).attr('class', `${currentClass} future`);
+      }
+    });
+  }
 });
